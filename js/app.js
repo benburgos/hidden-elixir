8987ec4f5bb8e5b7fb2360eb80459fb7778049f2
Ben Burgos <ben.a.burgos@gmail.com>
2022-03-23 22:23:27 -0700

    adds preliminary styling

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 const ingredients = document.querySelector('#ingredients');
+let ingredientList = [];
+let selectedIngredients = [];
 const drinklist = document.querySelector('#drinklist');
+let filteredDrinks = [];
 
 $.ajax('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
     .then((data) => {
         for(let ingredient in data.drinks){
+            ingredientList.push(data.drinks[ingredient].strIngredient1)
             const $button = $('<button>');
-            $button.text(data.drinks[ingredient].strIngredient1)
+            $button.text(ingredientList[ingredient])
             $(ingredients).append($button)
         }
     });
@@ -13,6 +17,7 @@ $.ajax('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
 $.ajax('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin')
     .then((data) => {
         for(let drink in data.drinks){
+            filteredDrinks.push(data.drinks[drink].strDrink)
             const $li = $('<li>');
             $li.text(data.drinks[drink].strDrink)
             $('ul').append($li)
