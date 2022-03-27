3554c8cd496543373890c6860c7ed15cbdfaeee3
Ben Burgos <ben.a.burgos@gmail.com>
2022-03-26 18:11:53 -0700

    adds style to drink detail page

--- drink/js/app.js
+++ drink/js/app.js
@@ -8,37 +8,54 @@ const imgHolder = $('#drink-image')
 const imgSource = $('#drink-image-source')
 const drinkGlass = $('#drink-glass-type')
 const drinkIngredients = $('#drink-ingredients')
+const drinkMeasurements = $('#drink-measurements')
 const drinkInstructions = $('#drink-instructions')
 const ingredients = [];
+const measurements = [];
 
 $.ajax(url)
     .then((data) => {
         h1.text(data.drinks[0].strDrink);
         header.append(h1);
-        imgHolder.text(`Image Placeholder: ${data.drinks[0].strDrinkThumb}`);
-        imgSource.text(`Source Placeholder: ${data.drinks[0].strImageSource}`);
-        drinkGlass.text(`Glass Placeholder: ${data.drinks[0].strGlass}`);
+        imgHolder.html(`<img src="${data.drinks[0].strDrinkThumb}" alt="${data.drinks[0].strDrink}"/>`)
+        imgSource.html(`<a href="${data.drinks[0].strImageSource}" target="_blank">Image Source</a>`);
+        drinkGlass.text(`Glass Type Needed: ${data.drinks[0].strGlass}`);
         ingredients.push(
             data.drinks[0].strIngredient1,
+            data.drinks[0].strMeasure1,
             data.drinks[0].strIngredient2,
+            data.drinks[0].strMeasure2,
             data.drinks[0].strIngredient3,
+            data.drinks[0].strMeasure3,
             data.drinks[0].strIngredient4,
+            data.drinks[0].strMeasure4,
             data.drinks[0].strIngredient5,
+            data.drinks[0].strMeasure5,
             data.drinks[0].strIngredient6,
+            data.drinks[0].strMeasure6,
             data.drinks[0].strIngredient7,
+            data.drinks[0].strMeasure7,
             data.drinks[0].strIngredient8,
+            data.drinks[0].strMeasure8,
             data.drinks[0].strIngredient9,
+            data.drinks[0].strMeasure9,
             data.drinks[0].strIngredient10,
+            data.drinks[0].strMeasure10,
             data.drinks[0].strIngredient11,
+            data.drinks[0].strMeasure11,
             data.drinks[0].strIngredient12,
+            data.drinks[0].strMeasure12,
             data.drinks[0].strIngredient13,
+            data.drinks[0].strMeasure13,
             data.drinks[0].strIngredient14,
+            data.drinks[0].strMeasure14,
             data.drinks[0].strIngredient15,
+            data.drinks[0].strMeasure15,
             );
         filteredIngredients = ingredients.filter(element => {
             return element !== null;
         })
-        drinkIngredients.text(`Ingredients Placeholder: ${filteredIngredients.join(', ')}`);
+        drinkIngredients.text(`Ingredients: ${filteredIngredients.join(' ')}`);
         console.log(filteredIngredients);
-        drinkInstructions.text(`Instructions Placeholder: ${data.drinks[0].strInstructions}`);
+        drinkInstructions.text(`Instructions: ${data.drinks[0].strInstructions}`);
     })
